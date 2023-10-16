fefeb6ea08245703a3a2cc3f5e66e302d02ec457
‘Inna’ <khagleeva_inna@mail.ru>
2023-10-16 18:05:12 +0300

    2.16 Начинаем программировать

---
+++ src/pages/App/index.tsx
@@ -0,0 +1,23 @@
+import MainPage from '../Main';
+
+export type Place = {
+  mark: string;
+  image: string;
+  price: number;
+  priceText: string;
+  bookmark?: boolean;
+  rating?: number;
+  name: string;
+  type: string;
+  id?: number;
+};
+
+type AppProps = {
+  places: Place[];
+};
+
+function App({ places }: AppProps): JSX.Element {
+  return <MainPage places={places} />;
+}
+
+export default App;
