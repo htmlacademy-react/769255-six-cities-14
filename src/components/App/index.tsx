41e3820ab6794d1d59461e8b2f8bb964f35180f1
Инна Хаглеева <37747433+InCheese@users.noreply.github.com>
2023-10-17 20:43:34 +0300

    Начинаем программировать (#3)
    * 2.16 Начинаем программировать
    
    * 2.16 Исправлена ошибки линтинга
    
    * App перенесен в components
    
    * По замечаниям

---
+++ src/components/App/index.tsx
@@ -0,0 +1,23 @@
+import MainPage from '../../pages/Main';
+
+export type Place = {
+  mark: string;
+  image: string;
+  price: number;
+  priceText: string;
+  bookmark?: boolean;
+  rating?: number;
+  name: string;
+  type: string;
+  id?: number;
+};
+
+type AppProps = {
+  places: Place[];
+};
+
+function App({ places }: AppProps) {
+  return <MainPage places={places} />;
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './components/App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+const places = [
+  {
+    mark: 'Premium',
+    image: 'img/apartment-01.jpg',
+    price: 120,
+    priceText: 'night',
+    name: 'Beautiful &amp; luxurious apartment at great location',
+    type: 'Apartment',
+    id: 1,
+  },
+
+  {
+    mark: '',
+    image: 'img/room.jpg',
+    price: 80,
+    priceText: 'night',
+    name: 'Wood and stone place',
+    type: 'Room',
+    id: 2,
+  },
+
+  {
+    mark: '',
+    image: 'img/apartment-02.jpg',
+    price: 132,
+    priceText: 'night',
+    name: 'Canal View Prinsengracht',
+    type: 'Apartment',
+    id: 3,
+  },
+
+  {
+    mark: 'Premium',
+    image: 'img/apartment-03.jpg',
+    price: 180,
+    priceText: 'night',
+    name: 'Nice, cozy, warm big bed apartment',
+    type: 'Apartment',
+    id: 4,
+  },
+];
+
 root.render(
   <React.StrictMode>
-    <h1>Hello, World!</h1>
+    <App places={places} />
   </React.StrictMode>
 );
